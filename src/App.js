c5f84da176687523fe930cbb06d3415dbbb34968
ramon <ramon@weprovide.com>
2021-03-03 16:34:11 +0100

    .

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload hallo?.
+          Edit <code>src/App.js</code> and save to reload ttt.
         </p>
         <a
           className="App-link"
