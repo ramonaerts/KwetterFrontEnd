0b22d3eb85deb3dace00fead664640248acc5f77
ramonaerts <ramon.aerts@gmail.com>
2021-03-17 17:09:17 +0100

    added simple login form to test backend

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,24 @@
-import React from "react";
-import "./index.css"
+import React, { Component } from "react";
+import { Container, Form, Button } from "react-bootstrap"
+import "./index.css";
 
-export default function Login() {
-    return (
-      <div>
-        Test
-      </div>
-    );
+export default class Login extends Component {
+    render(){
+      return (
+        <div className="loginContainer">
+          <Form className="loginform">
+            <h3>Login</h3>
+            <Form.Group>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Email address" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button className="loginButton" type="submit">Log in</Button>
+          </Form>
+        </div>
+      );
+    }
   }
