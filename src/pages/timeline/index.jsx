300ca294dd4e699df2c2fa76872fe8c75a197d38
ramonaerts <ramon.aerts@gmail.com>
2021-03-27 02:13:45 +0100

    tweet button and tweet modal front-end working

--- src/pages/timeline/index.jsx
+++ src/pages/timeline/index.jsx
@@ -1,31 +1,63 @@
 import React, { Component } from "react";
-import { Container, Form, Button } from "react-bootstrap"
+import { Container, Form, Button, Modal } from "react-bootstrap"
 import "./index.css";
 import Header from "../../components/header";
 import TweetCard from "../../components/tweet-card";
 
 export default class Timeline extends Component {
-    render(){
-        return (
-          <div>
-            <div className="header">
-              <Header pageName="Timeline" />
-            </div>   
-            <div className="timeline-container">         
-              <div className="left">
+  constructor() {
+    super()
+    this.state = {
+      show: false
+    };
+  }
 
-              </div>
-              <div className="middle">
-                <TweetCard/>
-                <TweetCard/>
-                <TweetCard/>
-                <TweetCard/>
-              </div>
-              <div className="right">
+  handleClose = () => {
+		this.setState({ show: false });
+	}
 
-              </div>
+	handleShow = () => {
+		this.setState({ show: true });
+  }
+
+  async tweet(){
+
+  }
+  
+  render(){
+      return (
+        <div>
+          <div className="header">
+            <Header pageName="Timeline" />
+          </div>   
+          <div className="timeline-container">         
+            <div className="left">
+              <Button className="tweet-button" onClick={this.handleShow}>Tweet</Button>
+              <Modal className="tweet-modal" show={this.state.show} onHide={this.handleClose}>
+                <Modal.Header>
+                  <Modal.Title>Tweet</Modal.Title>
+                </Modal.Header>
+                <Form onSubmit={this.tweet}>
+                <Modal.Body>
+                  <Form.Control className="send-tweet-content" name="tweet" type="text" placeholder="Wat houdt je bezig?" />
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button className="send-button" type="submit">Tweet</Button>
+                </Modal.Footer>
+                </Form>
+              </Modal>
+            </div>
+            <div className="middle">
+              <TweetCard/>
+              <TweetCard/>
+              <TweetCard/>
+              <TweetCard/>
+            </div>
+            <div className="right">
+
             </div>
           </div>
-        );
-      }
+        </div>
+      );
+    }
 }
